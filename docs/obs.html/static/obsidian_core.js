800f945c901c963d265aaed3dae3c96bebe80618
george-a-shoemaker <george.a.shoemaker@gmail.com>
2025-01-01 16:29:12 -0800

    Index title

--- docs/obs.html/static/obsidian_core.js
+++ docs/obs.html/static/obsidian_core.js
@@ -13,7 +13,7 @@ var CONFIG_CLOSE_LEFT_PANE_IF_EMPTY = 0;
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '254456800562134975260900220612259107628'                       // used to check whether the localStorage data is stale
+var gzip_hash = '294292675095528989796962684887985243836'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
